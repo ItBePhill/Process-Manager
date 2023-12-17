8eca77c2efe0a9b93be4038a2bce9b033db2271e
Phill <phillipwood01@gmail.com>
2023-12-17 17:14:20 +0000

    idk

--- ChangeJSON.py
+++ ChangeJSON.py
@@ -3,13 +3,29 @@
 import json
 import os
 import logging
-logging.basicConfig(filename='ChangeJson.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level = logging.INFO)
+from datetime import date
+logpath = os.path.join(os.path.dirname(__file__), "Logs")
+logs = len(os.listdir(logpath))
+logging.basicConfig(filename=f'{logpath}\{date.today()} Log.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
 logging.info("Started ChangeJSON Successfully!")
+
+
+
 file = os.path.join(os.path.dirname(__file__), "processes.json")
+if not os.path.exists(file):
+    logging.info("file not found creating processes.json")
+    try:
+        with open("processes.json", "w") as f:
+            json.dump({}, f)
+    except Exception as e:
+        logging.error("Something Went Wrong:", exc_info=e)
+        quit(0)
+    logging.info("Successfully created processes.json")
+
 def Edit(change):
     logging.info(f"Changing file to: {change}")
     try:
-        with open(file, "r+") as f:
+        with open(file, "w") as f:
             json.dump(change, f)
     except Exception as e:
         logging.error(f"Something Went Wrong:", exc_info= e)
@@ -17,3 +33,12 @@ def Edit(change):
 
     logging.info(f"Successfully Changed to: {change}")
     
+def Delete():
+    logging.info(f"Deleting processes.json")
+    try:
+        os.remove(file)
+    except Exception as e:
+        logging.error("Something Went Wrong:", exc_info = e)
+        return None
+    
+    logging.info("Successfully Deleted Processes.json")
--- Console UI.py
+++ Console UI.py
@@ -1,4 +1,13 @@
 #Console UI.py, Will be what gives Main.py what needs to be changed
 #Ver 0.1
-from ChangeJSON import *
+import logging
+from datetime import date
+import os
+logpath = os.path.join(os.path.dirname(__file__), "Logs")
+logs = len(os.listdir(logpath))
 
+if not os.path.exists(logpath):
+    os.makedirs(logpath)
+from ChangeJSON import *
+logging.basicConfig(filename=f'{logpath}\{logs+1}{date.today()} Log.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
+logging.info("Started Console UI Successfully!")
